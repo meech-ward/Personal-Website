0ce45cee2ce266e188cc1a0dc85f8a4748c0f1fa
Sam Meech-Ward <sam@meech-ward.com>
2017-07-18 21:10:11 -0700

    Add ejs
    Add the ejs library and move all of the html files from public into a new views directory. Change the html extensions to ejs.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080; // default port 8080
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.send("Hello iOS Developers!");
