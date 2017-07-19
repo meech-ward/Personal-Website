12ebc891e1125db6684f7eb6b6ae12141cd67474
Sam Meech-Ward <sam@meech-ward.com>
2017-07-18 21:03:18 -0700

    Serve up static html files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080; // default port 8080
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
   res.send("Hello iOS Developers!");
 });
