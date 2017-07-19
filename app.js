7c2cacf37595b9aa88c9977872ed65dc3e607f3e
Sam Meech-Ward <sam@meech-ward.com>
2017-07-18 20:52:53 -0700

    Create a simple express app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080; // default port 8080
+
+app.get("/", (req, res) => {
+  res.send("Hello iOS Developers!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
